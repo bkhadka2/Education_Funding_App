7e4251f54596db4d4c6209b9262dc61d312f2658
bkhadka2 <bishalkhadka1600@gmail.com>
2021-02-14 04:08:52 -0700

    Project Added

--- DonationPage/config/database.js
+++ DonationPage/config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb+srv://bishal:bishal@donationcluster.cakhv.mongodb.net/<dbname>?retryWrites=true&w=majority'
+        mongoURI: 'mongodb+srv://removed-for-security-reason'
     }
 }
 else {
